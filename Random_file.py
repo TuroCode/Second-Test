2a9fdea0b08f1715c2274f265f413f8f0af31d35
Arturo <rulla6132@gmail.com>
2026-05-10 13:18:04 +0200

    Silly

--- Random_file.py
+++ Random_file.py
@@ -2,10 +2,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 x=np.arange(1,10,0.01)
-y=x**3
+y=x**4
 
 plt.plot(x,y)
 plt.xlabel("x")
 plt.ylabel("y")
 plt.show()
 
+"This is just a sily change lol"
